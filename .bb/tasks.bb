0f71e166a30fa683ba64c19cd91873d44a4c26d6
Michiel Borkent <michielborkent@gmail.com>
2023-09-04 20:33:31 +0200

    wip

--- .bb/tasks.bb
+++ .bb/tasks.bb
@@ -5,13 +5,15 @@
 
 (defn watch-cljs []
   (let [watch (requiring-resolve 'pod.babashka.fswatcher/watch)
-        ret (watch "."
+        ret (watch "src-squint"
                    (fn [{:keys [type path]}]
                      (when (and (#{:write :write|chmod :create} type)
-                                (str/ends-with? path ".cljs"))
+                                (str/ends-with? path ".cljs")
+                                ;; emacs shit:
+                                (not (str/includes? path ".#")))
                        (shell {:continue true
                                :err :inherit
-                               :std :inherit} "yarn squint compile" path)))
+                               :std :inherit} "yarn squint compile --output-dir public/squint/js" path)))
                    {:recursive true})]
     (println (str "Started watching: " ret))
     @(promise)))
---
+++ src-squint/nextjournal/clojure_mode/chars.cljs
@@ -0,0 +1,30 @@
+(ns nextjournal.clojure-mode.chars
+  (:require ["@codemirror/text" :as text]))
+
+(defn pair-lookup [char-pairs ^string char]
+  (let [end (count char-pairs)
+        ch (text/codePointAt char 0)]
+    (loop [i 0]
+      (cond (>= i end) (text/fromCodePoint (if (< ch 128) ch (inc ch)))
+            (== ch (.charCodeAt char-pairs i)) (.charAt char-pairs (inc i))
+            :else (recur (+ i 2))))))
+
+(defn backspace? [^number code] (== code 8))
+
+(defn next-char [^js doc ^number pos]
+  (let [^string next (.sliceString doc pos (+ pos 2))]
+    (.slice next 0 (text/codePointSize (text/codePointAt next 0)))))
+
+(defn prev-char [^js doc ^number pos]
+  (if (pos-int? pos)
+    (.sliceString doc (dec pos) pos)
+    ""))
+
+(def whitespace? (->> [" " \n \r ","]
+                      (map #(.charCodeAt ^string % 0))
+                      (set)))
+
+(comment
+  ;; is there a way to iterate from a position, character by character?
+  (defn pos-when [doc dir pred]
+    ))
