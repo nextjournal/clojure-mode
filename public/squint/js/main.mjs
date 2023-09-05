c6966d78305d53c1d8ed575a18a3185484f45b1f
Michiel Borkent <michielborkent@gmail.com>
2023-09-05 17:34:19 +0200

    wip

--- public/squint/js/main.mjs
+++ public/squint/js/main.mjs
@@ -5,5 +5,6 @@ import './src-squint/nextjournal/clojure_mode/extensions/match_brackets.mjs';
 import './src-squint/nextjournal/clojure_mode/extensions/formatting.mjs';
 import './src-squint/nextjournal/clojure_mode/extensions/selection_history.mjs';
 import './src-squint/nextjournal/clojure_mode/commands.mjs';
+import './src-squint/nextjournal/clojure_mode/keymap.mjs';
 
 console.log('hello');
