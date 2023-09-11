adb9bf240af1297399ac21403a802685c2327f57
Michiel Borkent <michielborkent@gmail.com>
2023-09-11 12:26:36 +0200

    wip

--- public/squint/js/main.mjs
+++ public/squint/js/main.mjs
@@ -6,16 +6,19 @@ import './src-squint/nextjournal/clojure_mode/extensions/formatting.mjs';
 import './src-squint/nextjournal/clojure_mode/extensions/selection_history.mjs';
 import './src-squint/nextjournal/clojure_mode/commands.mjs';
 import './src-squint/nextjournal/clojure_mode/keymap.mjs';
-import './src-squint/nextjournal/clojure_mode.mjs';
+import { default_extensions } from './src-squint/nextjournal/clojure_mode.mjs';
 
 import { EditorView } from  '@codemirror/view';
 import { EditorState } from  '@codemirror/state';
+import { syntaxHighlighting, defaultHighlightStyle } from '@codemirror/language';
 
 
-let state = EditorState.create({doc: "(+ 1 2 3)"});
+console.log(default_extensions);
+
+let extensions = [ (syntaxHighlighting(defaultHighlightStyle)), ...default_extensions.slice(0,1) ]
+let state = EditorState.create( {doc: "(+ 1 2 3)",
+                                 extensions: extensions });
 let editorElt = document.querySelector('#editor');
 let editor = new EditorView({state: state,
                              parent: editorElt });
 
-console.log();
-console.log('hello');
