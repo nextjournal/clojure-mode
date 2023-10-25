80ec87cc238dbaedc154687b954215770fd2ac69
Michiel Borkent <michielborkent@gmail.com>
2023-10-25 15:11:50 +0200

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+import analyze from "rollup-plugin-analyzer";
+
 export default {
   optimizeDeps: {
     // avoids loading deps multiple time
     exclude: ['prosemirror-model', 'y-prosemirror', 'y-websocket'],
     // root: "demo"
-  }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [analyze()]
+    }
+  },
 };
