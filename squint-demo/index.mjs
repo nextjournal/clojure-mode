7a3137ec0dc4db3cf146052146565cb46bfa7eb5
Andrea Amantini <lo.zampino@gmail.com>
2023-11-01 11:35:14 +0100

    Squint CDN demo tweaks

--- squint-demo/index.mjs
+++ squint-demo/index.mjs
@@ -1,12 +1,8 @@
-console.log("start")
-
 import { default_extensions, complete_keymap } from '@nextjournal/clojure-mode';
 
 import { EditorView, drawSelection, keymap } from  '@codemirror/view';
 import { EditorState } from  '@codemirror/state';
 import { syntaxHighlighting, defaultHighlightStyle, foldGutter } from '@codemirror/language';
-
-console.log("imported")
 
 let theme = EditorView.theme({
   ".cm-content": {whitespace: "pre-wrap",
@@ -37,7 +33,20 @@ let extensions = [
                    ...default_extensions
                  ];
 
-let state = EditorState.create({doc: "(+ 1 2 3)",
+let state = EditorState.create({doc: `(comment
+  (fizz-buzz 1)
+  (fizz-buzz 3)
+  (fizz-buzz 5)
+  (fizz-buzz 15)
+  (fizz-buzz 17)
+  (fizz-buzz 42))
+
+(defn fizz-buzz [n]
+  (condp (fn [a b] (zero? (mod b a))) n
+    15 "fizzbuzz"
+    3  "fizz"
+    5  "buzz"
+    n))`,
                                  extensions: extensions });
 let editorElt = document.querySelector('#editor');
 let editor = new EditorView({state: state,
